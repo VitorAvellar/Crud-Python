9dff158eaa438eb40e9ebe3945a4874a1674be65
Vitor Avellar <vitor07sa@gmail.com>
2024-07-24 23:30:49 -0300

    24/07
    Criação do update e delete
    Criação das funções de cada crud

--- crud.py
+++ crud.py
@@ -8,23 +8,63 @@ conexao = mysql.connector.connect(
     database = 'bdcrud.py',
 
 )
+cursor = conexao.cursor()
 
 #create
-cursor = conexao.cursor()
-nome_produto = input('Digite o nonme do produto: ')
-valor = input(f'digite o valor do {nome_produto}: ')
+def add_produto(nome_produto, valor):
+    cursor = conexao.cursor()
 
-comando = f'INSERT INTO vendas (nome_produto, valor) VALUES ("{nome_produto}", {valor})'
-cursor.execute(comando)
-conexao.commit()
+    nome_produto = input('Digite o nonme do produto: ')
+    valor = input(f'digite o valor do {nome_produto}: ')
+    comando = (f'INSERT INTO vendas (nome_produto, valor) VALUES (?, ?)' , (nome_produto, valor))
+    cursor.execute(comando)
+    conexao.commit()
+    conexao.close()
+    print('Produto cadastrado com sucesso!!')
+
 
 #read
+def read_produto():
+    cursor = conexao.cursor()
 
-comando = f'SELECT * FROM vendas;'
-cursor.execute(comando)
-resultado = cursor.fetchall()
-print(resultado)
+    comando = f'SELECT * FROM vendas;'
+    cursor.execute(comando)
+    resultado = cursor.fetchall()
+    conexao.close()
+    print('produtos: ')
+    for produtos in resultado:
+        print(resultado)
+    print()
 
+#update
+def update_produtos(nome_produto):
+    cursor = conexao.cursor()
+
+    produto_find = input('Digite o nome do produto que será atualizado: ')
+    if(produto_find == nome_produto):
+        novo_valor = input(f'Digite o novo valor do(a) {produto_find}: ')
+        comando = f'UPDATE vendas SET valor = {novo_valor} WHERE nome_produto = "{produto_find}"'
+
+    else:
+        ('produto não encontrado')
+
+    cursor.execute(comando)
+    conexao.commit()
+    conexao.close()
+
+#rever o código
+
+
+#delete
+def deleatar_produtos():
+    cursor = conexao.cursor()
+
+    delete_produtos = input('Qual produto deseja deletar? ')
+    comando = f'DELETE FROM vendas WHERE nome_produto = "{delete_produtos}"'
+
+    cursor.execute(comando)
+    conexao.commit()
+    conexao.close()
 
 cursor.close()
 conexao.close()
